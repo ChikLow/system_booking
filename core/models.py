f120b86b0c8ce63360e6eac0a8165eb5d571c103
Roman <romanshtanhret@gmail.com>
2025-06-03 15:20:10 +0300

    did half on home lesson

--- core/models.py
+++ core/models.py
@@ -2,18 +2,42 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Room(models.Model):
+    class Level(models.TextChoices):
+        STANDARD = "STD", "Standard"
+        PREMIUM = "PRM", "Premium"
+        VIP = "VIP", "VIP"
+
     number = models.IntegerField()
-    capacity = models.IntegerField()
     location = models.TextField()
+    level = models.CharField(max_length=3, choices=Level.choices)
+
+    LEVEL_PRICES = {
+        Level.STANDARD: 500,
+        Level.PREMIUM: 1000,
+        Level.VIP: 2500,
+    }
+
+    LEVEL_CAPACITY = {
+        Level.STANDARD: 2,
+        Level.PREMIUM: 4,
+        Level.VIP: 8,
+    }
+
+    @property
+    def price(self):
+        return self.LEVEL_PRICES.get(self.level, 0)
+
+    @property
+    def capacity(self):
+        return self.LEVEL_CAPACITY.get(self.level, 0)
 
     def __str__(self):
-        return f"Room #{self.number} - #{self.capacity}"
-    
+        return f"Room #{self.number} - capacity {self.capacity} - price {self.price}$"
+
     class Meta:
         verbose_name = "Room"
         verbose_name_plural = "Rooms"
         ordering = ["number"]
-
 
 
 class Booking(models.Model):
--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from core.models import Room, Booking
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    context = {
+        "render_string": "Hello, world!"
+    }
+
+    return render(request, template_name="booking/index.html", context=context)
+
+
+def room_list(request):
+    rooms = Room.objects.all()
+    context = {
+        "rooms": rooms,
+    }
+
+    return render(request, template_name="booking/room_list.html", context=context)
